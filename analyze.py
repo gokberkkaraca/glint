0af7df21f9040412fa46cd03b80aba2cb03663eb
Gökberk Karaca <gokberk.karaca@outlook.com>
2018-07-23 16:59:19 -0400

    Improve analysis script

--- analyze.py
+++ analyze.py
@@ -14,6 +14,8 @@ for lint in glob.glob('lints/lint*'):
 # Parse result of each certificate to update lint dictionary
 for file in glob.glob('pem_certificates/certs_newer_2017/*.json'):
     file_content = open(file).read()
+    if file_content == "":
+        continue
     json_file = json.loads(file_content)
     for key in list(json_file):  # Modify lint message ignore first 3 characters
         json_file[key[2:]] = json_file[key]
@@ -33,8 +35,29 @@ for file in glob.glob('pem_certificates/certs_newer_2017/*.json'):
         elif json_file[lint]['result'] == "NE":
             lint_dictionary[lint]["NE"] += 1
 
+# Find highest info
+highest_pass_lint = list(lint_dictionary.keys())[0]
+highest_info_lint = list(lint_dictionary.keys())[0]
+highest_warn_lint = list(lint_dictionary.keys())[0]
+highest_error_lint = list(lint_dictionary.keys())[0]
+
+for lint in lint_dictionary:
+    if lint_dictionary[lint]["pass"] > lint_dictionary[highest_pass_lint]["pass"]:
+        highest_pass_lint = lint
+    if lint_dictionary[lint]["warn"] > lint_dictionary[highest_warn_lint]["warn"]:
+        highest_warn_lint = lint
+    if lint_dictionary[lint]["notice"] > lint_dictionary[highest_info_lint]["notice"]:
+        highest_info_lint = lint
+    if lint_dictionary[lint]["error"] > lint_dictionary[highest_error_lint]["error"]:
+        highest_error_lint = lint
+
 # Print results to console and a file
-result = json.dumps(lint_dictionary, indent=4)
-with open("result.txt", "w") as file:
+result = json.dumps(lint_dictionary, indent=4) + "\n"
+with open("certs_newer_2017.txt", "w") as file:
     file.write(result)
 print(result)
+
+print("Highest pass lint: {}, {}".format(highest_pass_lint, lint_dictionary[highest_pass_lint]["pass"]))
+print("Highest info lint: {}, {}".format(highest_info_lint, lint_dictionary[highest_info_lint]["notice"]))
+print("Highest warn lint: {}, {}".format(highest_warn_lint, lint_dictionary[highest_warn_lint]["warn"]))
+print("Highest error lint: {}, {}".format(highest_error_lint, lint_dictionary[highest_error_lint]["error"]))
