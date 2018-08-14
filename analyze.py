53e76ae2d3bf0efc5cfa6e382b4a240d70ff5205
Gökberk Karaca <gokberk.karaca@outlook.com>
2018-08-14 09:53:13 -0400

    Analyze success rates on different dates

--- analyze.py
+++ analyze.py
@@ -1,4 +1,5 @@
 import sqlite3
+import json
 
 
 db = sqlite3.connect('lint_results.db')
@@ -23,41 +24,40 @@ lints_to_be_analyzed_list = [
 ]
 print("Registered the lints to be analyzed list")
 
-ca_list = [row[0] for row in db.execute("SELECT DISTINCT certificate_issuer FROM certificates;")]
+ca_list = [row[0] for row in db.execute(
+    "SELECT DISTINCT certificate_issuer FROM certificates where certificate_date > '2016';")]
 print("CA list retrieved from database")
 
 years = list(range(2015, 2019))
 months = list(range(1, 13))
+dates = ['2016-01', '2016-04', '2016-07', '2016-10', '2017-01', '2017-04']
 
-filter_results = False
-
+all_results = {}
 for ca in ca_list:
-    print("\nAnalysis results for: ", ca)
+    all_results.update({ca: {}})
     for lint in lints_to_be_analyzed_list:
-        certificates_issued_by_ca = [row for row in db.execute("SELECT certificate_id, lint_name, certificate_date, result FROM certificates NATURAL JOIN results WHERE certificate_issuer=? AND lint_name LIKE '%"+ lint + "%'", (ca,))]
-        result_list = list(map(lambda item: item[3], certificates_issued_by_ca))
-        total = len(result_list)
-        _error = len(list(filter(lambda item: item=='error', result_list)))
-        _pass = len(list(filter(lambda item: item=='pass', result_list)))
-        _info = len(list(filter(lambda item: item=='info', result_list)))
-        _warn = len(list(filter(lambda item: item=='warn', result_list)))
-        _NA = len(list(filter(lambda item: item=='NA', result_list)))
-        _NE = len(list(filter(lambda item: item=='NE', result_list)))
-        if total != 0:
-            success_rate = (_pass + _NA + _NE)/total
-        else:
-            success_rate = 1.0
-        check = False
-        if success_rate != 1.0:
-            check = True
-
-        if filter_results:
-            if check:
-                print("{}, Total: {}, Error: {}, Pass: {}, Info: {}, Warn: {}, NA: {}, NE: {}, Success: {}"
-                      .format(lint, total, _error, _pass, _info, _warn, _NA, _NE, success_rate))
-        else:
-            print("{}, Total: {}, Error: {}, Pass: {}, Info: {}, Warn: {}, NA: {}, NE: {}, Success: {}"
-                  .format(lint, total, _error, _pass, _info, _warn, _NA, _NE, success_rate))
-    print("\n----------------------------------------------------------------------------------------------")
+        all_results[ca].update({lint: {}})
 
 
+for date in dates:
+    for ca in ca_list:
+        for lint in lints_to_be_analyzed_list:
+            certificates_issued_by_ca = [row for row in db.execute(
+                "SELECT certificate_id, lint_name, certificate_date, result FROM certificates NATURAL JOIN results "
+                "WHERE certificate_date > ? AND certificate_issuer=? AND lint_name LIKE '%" + lint + "%'", (date, ca,))]
+            result_list = list(map(lambda item: item[3], certificates_issued_by_ca))
+            total = len(result_list)
+            _error = len(list(filter(lambda item: item == 'error', result_list)))
+            _pass = len(list(filter(lambda item: item == 'pass', result_list)))
+            _info = len(list(filter(lambda item: item == 'info', result_list)))
+            _warn = len(list(filter(lambda item: item == 'warn', result_list)))
+            _NA = len(list(filter(lambda item: item == 'NA', result_list)))
+            _NE = len(list(filter(lambda item: item == 'NE', result_list)))
+
+            if total != 0:
+                success_rate = (_pass + _NA + _NE)/total
+            else:
+                success_rate = 1.0
+            all_results[ca][lint].update({date: success_rate})
+
+print(json.dumps(all_results, indent=4))
